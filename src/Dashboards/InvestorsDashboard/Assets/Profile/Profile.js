4619dac89e1f059f5609dd706ad0b83e31bc4374
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2020-07-11 23:40:57 +0530

    Update Profile.js

--- src/Dashboards/InvestorsDashboard/Assets/Profile/Profile.js
+++ src/Dashboards/InvestorsDashboard/Assets/Profile/Profile.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 import Name from "./Assets/Name"
 import About from "./Assets/About"
 export default class Profile extends Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			myProfile: {}
 		};
+	
 	}
 
 	componentDidMount() {
-		var id = '5f06cef3c99ce30f9aa4d169'
+		var id = '5f09f43ff0612e2e4cc7db54'
 		var persons;
 
 		axios.get(`http://localhost:8081/investor/profile/` + id)
@@ -22,12 +22,9 @@ export default class Profile extends Component {
 				this.setState({ myProfile: persons })
 			})
 	}
-
 	render() {
-		
 		return <div style={{ right: "25%", float: "right", position: "relative" }}>
 			<Name data={this.state.myProfile} />
 		</div>;
-		
 	}
 }
