1ed94d244e37b7248dee120003c5b115d71a5342
chaitrali-shinde <chai.shinde100@gmail.com>
2020-07-23 12:27:53 +0530

    footer fixes

--- src/FrontPage/Footer.js
+++ src/FrontPage/Footer.js
@@ -30,11 +30,11 @@ function Footer(props) {
                 <div className="col-12 col-sm-4 align-self-center">
                     <div className="text-center">
                         <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
-                        <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                        <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id=" style={{marginLeft: 8}}><i className="fa fa-facebook" ></i></a>
+                        <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/" style={{marginLeft: 8}}><i className="fa fa-linkedin" ></i></a>
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/" style={{marginLeft: 8}}><i className="fa fa-twitter"></i></a>
+                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/" style={{marginLeft: 8}}><i className="fa fa-youtube"></i></a>
+                        <a className="btn btn-social-icon" href="mailto:" style={{marginLeft: 8}}><i className="fa fa-envelope-o"></i></a>
                     </div>
                 </div>
             </div>
