88437237d565caf215b35d739e5167d066c3c0ba
manvendra <manvendrachavan@gmail.com>
2020-07-24 01:21:00 +0530

    Ad removed in mentor dashboard

--- src/Dashboards/CommunityDashboard/Assets/Advertise/AdvertiseRight.js
+++ src/Dashboards/CommunityDashboard/Assets/Advertise/AdvertiseRight.js
@@ -1,22 +1,22 @@
-import React, { useState, Component } from 'react';
-import Divider from '@material-ui/core/Divider';
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
+import React, { useState, Component } from "react";
+import Divider from "@material-ui/core/Divider";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
 import { withStyles } from "@material-ui/core/styles";
-import CardMedia from '@material-ui/core/CardMedia';
-import TextField from '@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
-import Cookies from 'js-cookie'
-const useStyles = theme => ({
+import CardMedia from "@material-ui/core/CardMedia";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import Cookies from "js-cookie";
+const useStyles = (theme) => ({
   root: {
     width: "20%",
     right: 0,
-    position: 'fixed',
+    position: "fixed",
     marginRight: 10,
   },
   media: {
-    height: 340
+    height: 340,
   },
 });
 
@@ -27,11 +27,11 @@ class AdvertiseRight extends Component {
     this.state = {
       Advert: {},
       image: null,
-      feedback: '',
-      adId: ''
+      feedback: "",
+      adId: "",
     };
-    this.getAdByDomain = this.getAdByDomain.bind(this)
-    this.sendFeedBack = this.sendFeedBack.bind(this)
+    this.getAdByDomain = this.getAdByDomain.bind(this);
+    this.sendFeedBack = this.sendFeedBack.bind(this);
   }
 
   componentDidMount() {
@@ -43,34 +43,37 @@ class AdvertiseRight extends Component {
     clearInterval(this.interval);
   }
   componentWillMount() {
-    this.getAdByDomain()
-  };
+    this.getAdByDomain();
+  }
 
   getAdByDomain() {
     var ads;
     var self = this;
-    axios.get(`http://54.237.17.61/advert/getAdByDomain`, { params: { domain: this.props.domain } })
-      .then(res => {
-        ads = res.data;
-        self.setState({ Advert: ads })
-        self.setState({ adId: ads.adId })
-        self.setState({ image: ads.image.data })
+    axios
+      .get(`http://54.237.17.61/advert/getAdByDomain`, {
+        params: { domain: this.props.domain },
       })
+      .then((res) => {
+        ads = res.data;
+        self.setState({ Advert: ads });
+        self.setState({ adId: ads.adId });
+        self.setState({ image: ads.image.data });
+      });
   }
 
   sendFeedBack() {
     var self = this;
-    axios.post('http://54.237.17.61/advert/addFeedback/', {
-      "username": Cookies.get('username'),
-      "feedbackBody": self.state.feedback,
-      "adId": self.state.adId
-    })
-      .then(function (response) {
-        self.setState({feedback:''})
-        console.log(response.data);
-        alert(response.data)
+    axios
+      .post("http://54.237.17.61/advert/addFeedback/", {
+        username: Cookies.get("username"),
+        feedbackBody: self.state.feedback,
+        adId: self.state.adId,
       })
-
+      .then(function (response) {
+        self.setState({ feedback: "" });
+        console.log(response.data);
+        alert(response.data);
+      });
   }
   render() {
     const { classes } = this.props;
@@ -78,7 +81,12 @@ class AdvertiseRight extends Component {
     return (
       <div className={classes.root}>
         <Card variant="outlined">
-          <Typography variant="h5" color='primary' style={{ margin: 10 }} gutterBottom>
+          <Typography
+            variant="h5"
+            color="primary"
+            style={{ margin: 10 }}
+            gutterBottom
+          >
             {this.state.Advert.header}
           </Typography>
           <CardMedia
@@ -86,25 +94,36 @@ class AdvertiseRight extends Component {
             image={`data:image/jpeg;base64,${this.state.image}`}
             title="Contemplative Reptile"
           />
-          <Typography style={{ padding: '2%' }} variant="body2" color="textSecondary" component="p">
+          <Typography
+            style={{ padding: "2%" }}
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
             {this.state.Advert.description}
           </Typography>
-          <TextField style={{ margin: 10, width: "100%", paddingRight: 20 }}
+          <TextField
+            style={{ margin: 10, width: "100%", paddingRight: 20 }}
             id="outlined-multiline-static"
             label="Feedback"
             multiline
             value={this.state.feedback}
             rows={4}
             variant="outlined"
-            onChange={(event) => { this.setState({ feedback: event.target.value }) }}
+            onChange={(event) => {
+              this.setState({ feedback: event.target.value });
+            }}
           />
           <Divider />
-          <div style={{ width: '100%', display: 'flex' }}>
-            <Button onClick={this.sendFeedBack} style={{ margin: 10, marginLeft: 'auto' }}>send</Button>
+          <div style={{ width: "100%", display: "flex" }}>
+            <Button
+              onClick={this.sendFeedBack}
+              style={{ margin: 10, marginLeft: "auto" }}
+            >
+              send
+            </Button>
           </div>
-
         </Card>
-
       </div>
     );
   }
--- src/Dashboards/MentorDashboard/Assets/Advertise/Advertise.js
+++ src/Dashboards/MentorDashboard/Assets/Advertise/Advertise.js
@@ -1,64 +1,140 @@
-import React, { useState, Component } from 'react';
-import Divider from '@material-ui/core/Divider';
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
+import React, { useState, Component } from "react";
+import Divider from "@material-ui/core/Divider";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
 import { withStyles } from "@material-ui/core/styles";
-import CardMedia from '@material-ui/core/CardMedia';
-import TextField from '@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
-const useStyles = theme => ({
+import CardMedia from "@material-ui/core/CardMedia";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import CardHeader from "@material-ui/core/CardHeader";
+import Cookies from "js-cookie";
+import { shadows } from "@material-ui/system";
+import { Link } from "react-router-dom";
+import { Container } from "@material-ui/core";
+
+const useStyles = (theme) => ({
   root: {
-      width:"15%",
-      margin:0,
-      position:'fixed',
-      height: "90%",
+    width: "15%",
+    margin: 0,
+    position: "fixed",
+    height: "90%",
   },
-  media:{
-    height:140
+  media: {
+    height: 140,
+  },
+  large: {
+    width: theme.spacing(11),
+    height: theme.spacing(11),
   },
 });
 
 class Advertise extends Component {
   constructor(props) {
-		super(props);
+    super(props);
 
-		this.state = {
-			members:[]
-		};
+    this.state = {
+      members: [],
+      image: null,
+      myProfile: {},
+    };
+    this.getImage = this.getImage.bind(this);
   }
-  
-  componentWillMount(){
-  };
 
+  componentWillMount() {
+    this.getImage();
+    this.getName();
+  }
+
+  getImage() {
+    var self = this;
+    var mem;
+    axios
+      .get(`http://54.237.17.61/management/mentor/photos/` + Cookies.get("id"))
+      .then((res) => {
+        mem = res.data;
+        self.setState({ image: mem });
+      });
+  }
+  showImage() {
+    return <img src={`data:image/jpeg;base64,${this.state.image}`} />;
+  }
+
+  getName() {
+    var persons;
+    axios
+      .get(`http://54.237.17.61/management/mentor/profile/` + Cookies.get("id"))
+      .then((res) => {
+        persons = res.data;
+        console.log(persons);
+        this.setState({ myProfile: persons });
+      });
+  }
 
   render() {
     const { classes } = this.props;
 
-  return (
-    <div className={classes.root}>
-      <Card variant="outlined">
-      <Typography variant="h5" color='primary' style={{ margin: 10 }} gutterBottom>
-                        Advertise
-							</Typography>
-      <CardMedia
-          className={classes.media}
-          image="http://blog.clickdimensions.com/wp-content/uploads/2017/05/BlogFeatureImage-5-Types-of-Online-Advertising-to-Generate-Leads.png"
-          title="Contemplative Reptile"
-        />
-            <TextField style={{margin:10,width:"100%",paddingRight:20}}
-          id="outlined-multiline-static"
-          label="Feedback"
-          multiline
-          rows={4}
-          variant="outlined"
-        />
-      <Divider/>
-     <Button style={{margin:10,}}>send</Button>
-      </Card>
-      
-    </div>
-  );
-}
+    return (
+      <div className={classes.root}>
+        <Link to="/mentorDashboard/Profile">
+          <Card style={{ marginTop: 30 }}>
+            <CardHeader
+              avatar={
+                <Avatar
+                  aria-label="Recipe"
+                  variant="circle"
+                  src={`data:image/jpeg;base64,${this.state.image}`}
+                  className={classes.large}
+                  style={{
+                    border: 4,
+                    marginLeft: 37,
+                    border: "2px solid rgba(0, 0, 0, 0.23)",
+                  }}
+                />
+              }
+            />
+            <center>
+              <Typography
+                variant="h7"
+                color="primary"
+                style={{ fontWeight: "bold" }}
+              >
+                <div>
+                  {" "}
+                  {" " +
+                    this.state.myProfile.firstName +
+                    " " +
+                    this.state.myProfile.lastName}
+                </div>
+              </Typography>
+            </center>{" "}
+            <br></br>
+            <Divider />
+            <Typography
+              variant="h8"
+              style={{ color: "#696969", marginLeft: 5 }}
+            >
+              Connections
+            </Typography>
+            <br />
+            <Typography
+              variant="h8"
+              style={{ marginLeft: 5, fontWeight: "bold" }}
+            >
+              Startup
+            </Typography>
+            <br />
+            <Typography
+              variant="h8"
+              style={{ marginLeft: 5, fontWeight: "bold" }}
+            >
+              Investor
+            </Typography>
+          </Card>
+        </Link>
+      </div>
+    );
+  }
 }
 export default withStyles(useStyles)(Advertise);
