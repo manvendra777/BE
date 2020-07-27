640eb5d169925efc150625f702e2fa268e6de68e
chaitrali-shinde <chai.shinde100@gmail.com>
2020-07-28 00:43:21 +0530

    sidebar

--- src/Dashboards/InvestorsDashboard/Assets/Advertise/Advertise.js
+++ src/Dashboards/InvestorsDashboard/Assets/Advertise/Advertise.js
@@ -1,64 +1,103 @@
-import React, { useState, Component } from 'react';
-import Divider from '@material-ui/core/Divider';
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
+import React, { useState, Component } from "react";
+import Divider from "@material-ui/core/Divider";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
 import { withStyles } from "@material-ui/core/styles";
-import CardMedia from '@material-ui/core/CardMedia';
-import TextField from '@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
-const useStyles = theme => ({
+import CardMedia from "@material-ui/core/CardMedia";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import CardHeader from "@material-ui/core/CardHeader";
+import Cookies from "js-cookie";
+import { shadows } from "@material-ui/system";
+import { Link } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import './sidebar.css'
+
+
+const useStyles = (theme) => ({
   root: {
-      width:"15%",
-      margin:0,
-      position:'fixed',
-      height: "90%",
+    width: "15%",
+    margin: 0,
+    position: "fixed",
+    height: "90%",
   },
-  media:{
-    height:140
+  media: {
+    height: 140,
+  },
+  large: {
+    width: theme.spacing(11),
+    height: theme.spacing(11),
   },
 });
 
 class Advertise extends Component {
   constructor(props) {
-		super(props);
-
-		this.state = {
-			members:[]
-		};
+    super(props);
+    console.log(Cookies.get("id"));
+    this.state = {
+      members: [],
+      image: null,
+      myProfile: {},
+    };
+    this.getImage = this.getImage.bind(this);
   }
-  
-  componentWillMount(){
-  };
 
+  componentWillMount() {
+    this.getImage();
+    this.getName();
+  }
+
+  getImage() {
+    var self = this;
+    var mem;
+    axios
+      .get(`http://54.237.17.61/management/investor/photos/` + Cookies.get("id"))
+      .then((res) => {
+        mem = res.data;
+        self.setState({ image: mem });
+      });
+  }
+  showImage() {
+    return <img src={`data:image/jpeg;base64,${this.state.image}`} />;
+  }
+
+  getName() {
+    var persons;
+    axios
+      .get(`http://54.237.17.61/management/investor/profile/` + Cookies.get("id"))
+      .then((res) => {
+        persons = res.data;
+        console.log(persons);
+        this.setState({ myProfile: persons });
+      });
+  }
 
   render() {
     const { classes } = this.props;
 
-  return (
-    <div className={classes.root}>
-      <Card variant="outlined">
-      <Typography variant="h5" color='primary' style={{ margin: 10 }} gutterBottom>
-                        Advertise
-							</Typography>
-      <CardMedia
-          className={classes.media}
-          image="http://blog.clickdimensions.com/wp-content/uploads/2017/05/BlogFeatureImage-5-Types-of-Online-Advertising-to-Generate-Leads.png"
-          title="Contemplative Reptile"
-        />
-            <TextField style={{margin:10,width:"100%",paddingRight:20}}
-          id="outlined-multiline-static"
-          label="Feedback"
-          multiline
-          rows={4}
-          variant="outlined"
-        />
-      <Divider/>
-     <Button style={{margin:10,}}>send</Button>
-      </Card>
-      
-    </div>
-  );
-}
+    return (
+      <div className={classes.root}>
+        <Link to="/investorDashboard/Profile">
+        <div className="container d-flex justify-content-center">
+        <div className="card p-3 py-4"  style={{background:"white"}}>
+        <div className="text-center"> <img src={`data:image/jpeg;base64,${this.state.image}`} style={{height: 100, width: 100}} className="rounded-circle" />
+    <h3 className="mt-2"> {" "}{" " +this.state.myProfile.firstName +" " +this.state.myProfile.lastName}</h3> 
+    <Divider variant="middle" /><br/>
+    <span className="mt-1 clearfix">{this.state.myProfile.investingCapacity}</span>
+    <div className="social-buttons mt-5"> 
+                        <a href="https://www.linkedin.com/" target="_blank"><button className="neo-button" onclick="location.href='http://www.example.com'" type="button"><i className="fa fa-linkedin fa-1x"></i> </button> </a>
+                        <a href="https://www.google.com/" target="_blank"><button className="neo-button"><i className="fa fa-google fa-1x"></i> </button></a>
+                        <a href="https://www.youtube.com/" target="_blank">   <button className="neo-button"><i className="fa fa-youtube fa-1x"></i> </button></a> 
+                        <a href ="https://www.twitter.com/" target="_blank"><button className="neo-button"><i className="fa fa-twitter fa-1x"></i> </button></a> 
+                    </div>
+            </div>
+            </div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
 }
 export default withStyles(useStyles)(Advertise);
